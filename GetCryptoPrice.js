7b40ab5daa26824ae573be4b81affb1f43552407
Interzoid <50465512+interzoid@users.noreply.github.com>
2021-08-31 15:28:42 -0700

    Create GetCryptoPrice.js

---
+++ GetCryptoPrice.js
@@ -0,0 +1,21 @@
+// Get API key at www.interzoid.com/register
+// npm install xmlhttprequest
+
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+const data = null;
+
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+	if (this.readyState === this.DONE) {
+		console.log(this.responseText);
+	}
+});
+
+xhr.open("GET", "https://api.interzoid.com/getcryptoprice?symbol=BTC");
+xhr.setRequestHeader("content-type", "application/json");
+xhr.setRequestHeader("x-api-key", "YOURAPIKEY");
+
+xhr.send(data);
